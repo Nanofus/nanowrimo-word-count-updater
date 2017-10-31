1416f1549477b7c3fbbd7c66397506b84802e6bb
Ville Talonpoika <talonvi@gmail.com>
2017-10-31 20:47:52 +0200

    1.0

--- nanowrimo-word-count-updater.py
+++ nanowrimo-word-count-updater.py
@@ -10,7 +10,7 @@ class PostWordCountCommand(sublime_plugin.TextCommand):
         sublime.status_message("Starting to update NaNoWriMo word count...")
 
         # Load settings
-        global_settings = sublime.load_settings("nanowrimo.sublime-settings")
+        global_settings = sublime.load_settings("nanowrimo-word-count-updater.sublime-settings")
         secret_key = global_settings.get('nanowrimo_secret_key', "")
         name = global_settings.get('nanowrimo_name', "")
 
@@ -34,22 +34,30 @@ class PostWordCountCommand(sublime_plugin.TextCommand):
         api_string = secret_key + name + str(count)
         hash = hashlib.sha1(api_string.encode("utf-8"))
 
+        # Request headers
+        headers = {
+            "Content-Length": 0
+        }
+
+        url = "https://nanowrimo.org/api/wordcount?hash=" + str(hash.hexdigest()) + "&name=" + name + "&wordcount=" + str(count)
+
         # Make the request
-        req = urllib.request.Request(url="https://nanowrimo.org/api/wordcount?hash=" + str(hash.hexdigest()) + "&name=" + name + "&wordcount=" + str(count), method="PUT")
+        req = urllib.request.Request(url=url, method="PUT", headers=headers)
         with urllib.request.urlopen(req) as f:
             pass
-        print(f.status)
-        print(f.reason)
+
         response_body = f.read()
-        print(response_body)
 
         # Check results
-        if ("{\"novel\"=>" in str(response_body)):
+        if (f.status == 200):
             sublime.status_message("NaNoWriMo word count updated to " + str(count) + " for user " + name)
+            sublime.message_dialog("NaNoWriMo word count updated to " + str(count) + " for user " + name)
             print("NaNoWriMo word count updated to " + str(count) + " for user " + name)
         else:
             sublime.status_message("Something went wrong with NaNoWriMo word count update, please check the console.")
             sublime.message_dialog("Something went wrong with NaNoWriMo word count update, please check the console.")
             print("NaNoWriMo: Something went wrong; response:")
+            print(f.status)
+            print(f.reason)
             print("Word count: " + str(count))
             print("Hash for API: " + hash.hexdigest())
